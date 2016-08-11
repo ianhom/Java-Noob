68e29cdc4c49a839419e45f71dd9e35185deecdd
Ian Hom <ianhom@163.com>
2016-08-11 17:34:40 +0800

    Update HelloWorld.java

--- Note_1/HelloWorld.java
+++ Note_1/HelloWorld.java
@@ -1,7 +1,7 @@
-public class HelloWorld 
+public class HelloWorld /* 类名要和文件名保持一致 */
 {
-    public static void main(String []args) 
+    public static void main(String []args)    /* 类中的main函数 */
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World");    /* main中的函数内容，打印hello world，类似printf */
     }
 } 
