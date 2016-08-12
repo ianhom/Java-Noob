b5832a1066c960ea54201871e532c967a6a4eb23
Ian Hom <ianhom@163.com>
2016-08-12 14:12:48 +0800

    Update HelloWorld.java

--- Note_1/HelloWorld.java
+++ Note_1/HelloWorld.java
@@ -1,6 +1,11 @@
-public class HelloWorld /* 类名要和文件名保持一致 */
+/* 类名要和文件名保持一致，文件名后缀为.java */
+/* 大小写敏感                                */ 
+/* 类名首字母大写,每个单词首字母大写         */
+public class HelloWorld 
 {
-    public static void main(String []args)    /* 类中的main函数 */
+    /* main是主方法，所有java程序由该主方法进入 */
+    /* 所有方法首字母小写，后续单词首字母大写   */
+    public static void main(String []args)
     {
         System.out.println("Hello World");    /* main中的函数内容，打印hello world，类似printf */
     }
