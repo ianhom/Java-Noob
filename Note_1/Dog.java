3b193190f2f8e84266dac7b34d7a0cd840880637
Ian Hom <ianhom@163.com>
2016-08-18 23:55:31 +0800

    Update Dog.java

--- Note_1/Dog.java
+++ Note_1/Dog.java
@@ -1,7 +1,7 @@
-public class Dog{
-    int dogage;
-    public Dog(String name){
-        System.out.println("Dog name is " + name);
+public class Dog{                /* 狗的类 */
+    int dogage;                  /* 狗的参数：年龄 */
+    public Dog(String name){     /* 与类名同名的是构造函数，在new时自动调用，无返回值 */
+        System.out.println("Dog name is " + name);  /* 在new一个狗的对象时，要求输出狗的名字 */
     }
     
     public void setAge(int age){
