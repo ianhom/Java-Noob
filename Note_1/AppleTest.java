cb58930bbc437c87c7f247fc48f4b2d04e5f236e
Ian Hom <ianhom@163.com>
2016-08-18 23:52:51 +0800

    Update AppleTest.java

--- Note_1/AppleTest.java
+++ Note_1/AppleTest.java
@@ -8,7 +8,7 @@ public class AppleTest{
     public static void main(String []args){
         Apple apple = new Apple();
         apple.size  = Apple.AppleSize.BIG;
-        System.out.println("Hello World");
-        System.out.println("Apple size is" + apple.size);
+        System.out.println("Hello World");  /* 纯字符串同C语言的printf，无需增加\n来换行 */
+        System.out.println("Apple size is" + apple.size);  /* 如果有参数需要打印，直接在双引号之外写明，通过加号连接 */
     }
 }
