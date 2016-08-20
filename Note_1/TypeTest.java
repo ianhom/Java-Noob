2a3b575eeebbcb7054246d73157fcdd938a455f7
Ian Hom <ianhom@163.com>
2016-08-20 14:23:01 +0800

    Create TypeTest.java

---
+++ Note_1/TypeTest.java
@@ -0,0 +1,9 @@
+public class TypeTest{
+    public static void main(String[] args){
+        System.out.println("基本类型：byte 二进制位数：" + Byte.SIZE);
+        System.out.println("包装类：java.lang.Byte");
+        System.out.println("最大值：Byte.MIN_VALUE="+ Byte.MIN_VALUE);
+        System.out.println("最小值：Byte.MAX_VALUE="+ Byte.MAX_VALUE);
+        System.out.println();
+    }
+}
