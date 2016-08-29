e97313d1bc8845d01adee52884fb1c149e88a872
Ian Hom <ianhom@163.com>
2016-08-29 17:16:09 +0800

    Update Student.java

--- Note_1/Student.java
+++ Note_1/Student.java
@@ -1,22 +1,27 @@
+/* 类为student */
 public class Student{
-    public String name;
-    private double score;
-    public Student(String stName){    /* 构造函数 */
+    public String name;    /* 成员变量               */
+    private double score;  /* 成员变量，仅在本类可见 */
+    
+    /* 构造函数 */
+    public Student(String stName){    
         name = stName;
     }
     
-    public void setScore(double stScore){   /* 方法：设置分数 */
+    /* 方法：设置分数 */
+    public void setScore(double stScore){   
         score = stScore;
     }
     
-    public void printSt(){     /* 方法：打印名字和分数 */
+    /* 方法：打印名字和分数 */
+    public void printSt(){     
         System.out.println("Name: " + name);
         System.out.println("Score: " + score);
     }
     
     public static void main(String args[]){
-        Student s = new Student("Ye");
-        s.setScore(100);
-        s.printSt();
+        Student s = new Student("Ye");      /* 创建一个student的对象s，生成的同时调用构造函数设置名字 */
+        s.setScore(100);                    /* 调用方法设置对象s的分数 */
+        s.printSt();                        /* 调用方法打印姓名和分数  */
     }
 }
