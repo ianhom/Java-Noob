a3963a4ee9cfdf458704d78f3774450c74abe115
Ian Hom <ianhom@163.com>
2016-08-30 13:31:31 +0800

    Create Student2.java

---
+++ Note_1/Student2.java
@@ -0,0 +1,8 @@
+public class Students2{
+    private static double score;
+    public static final String GOV = "管理"；
+    public static void main(String args[]){
+        score = 100;
+        System.out.println(GOV + "得分： " + score);
+    }
+}
