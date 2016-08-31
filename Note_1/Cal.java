90b31b85d8b153b2f199a057bcfe9ac23a1571f4
Ian Hom <ianhom@163.com>
2016-08-31 13:12:09 +0800

    Create Cal.java

---
+++ Note_1/Cal.java
@@ -0,0 +1,17 @@
+public class Cal{
+    public static void main(String args[]){
+        int a = 20;
+        int b = 10;
+        int c = 4;
+        int d = 5;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % c = " + (a % c));
+        System.out.println("b % c = " + (b % c));
+        System.out.println("d++ = " + (d++));
+        System.out.println("d-- = " + (d--));
+        System.out.println("++d = " + (++d));
+    }
+}
