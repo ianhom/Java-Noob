5bd6388afd3fa3a74f71989125c40ef1310350e6
Ian Hom <ianhom@163.com>
2016-08-31 13:17:11 +0800

    Update Cal.java

--- Note_1/Cal.java
+++ Note_1/Cal.java
@@ -1,9 +1,12 @@
-public class Cal{
-    public static void main(String args[]){
-        int a = 20;
+public class Cal{     /* 类名 */
+    public static void main(String args[]){    /* 主方法 */
+        /* 临时变量 */
+        int a = 20;    
         int b = 10;
         int c = 4;
         int d = 5;
+        
+        /* 打印运算结果 */
         System.out.println("a + b = " + (a + b));
         System.out.println("a - b = " + (a - b));
         System.out.println("a * b = " + (a * b));
@@ -15,3 +18,4 @@ public class Cal{
         System.out.println("++d = " + (++d));
     }
 }
+/* 普通运算基本与c类似 */
