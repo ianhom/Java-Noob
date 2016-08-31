f027f917461d62ad1ee9cb31d5440b5011d0bd4a
Ian Hom <ianhom@163.com>
2016-08-31 13:22:21 +0800

    Update Cal.java

--- Note_1/Cal.java
+++ Note_1/Cal.java
@@ -7,6 +7,7 @@ public class Cal{     /* 类名 */
         int d = 5;
         
         /* 打印运算结果 */
+        /* 算术运算 */
         System.out.println("a + b = " + (a + b));
         System.out.println("a - b = " + (a - b));
         System.out.println("a * b = " + (a * b));
@@ -16,6 +17,15 @@ public class Cal{     /* 类名 */
         System.out.println("d++ = " + (d++));
         System.out.println("d-- = " + (d--));
         System.out.println("++d = " + (++d));
+        
+        /* 关系运算 */
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a != b = " + (a != b));
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("a >= b = " + (a >= b));
+        System.out.println("a <= b = " + (a <= b));
+        
     }
 }
 /* 普通运算基本与c类似 */
