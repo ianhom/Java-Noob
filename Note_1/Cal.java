66c8f3144ed744258061382294c76f486fbeb8f5
Ian Hom <ianhom@163.com>
2016-09-01 17:14:42 +0800

    Update Cal.java

--- Note_1/Cal.java
+++ Note_1/Cal.java
@@ -26,6 +26,27 @@ public class Cal{     /* 类名 */
         System.out.println("a >= b = " + (a >= b));
         System.out.println("a <= b = " + (a <= b));
         
+        /* 逻辑运算 */
+        System.out.println("a & b = " + c );
+
+        c = a | b;       /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c );
+
+        c = a ^ b;       /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c );
+
+        c = ~a;          /*-61 = 1100 0011 */
+        System.out.println("~a = " + c );
+
+        c = a << 2;     /* 240 = 1111 0000 */
+        System.out.println("a << 2 = " + c );
+
+        c = a >> 2;     /* 15 = 1111 */
+        System.out.println("a >> 2  = " + c );
+  
+        c = a >>> 2;     /* 15 = 0000 1111 */
+        System.out.println("a >>> 2 = " + c );
+
     }
 }
 /* 普通运算基本与c类似 */
