4a3d673bff6362603570eec3531005fa72a3776a
Ian Hom <ianhom@163.com>
2016-09-03 09:57:46 +0800

    Update Cond.java

--- Note_1/Cond.java
+++ Note_1/Cond.java
@@ -2,7 +2,7 @@ public class Cond{
     public String name = "Ye";
     public static void main(String args[]){
         int a,b;
-        boolean c = name instanceOf String;
+        boolean c = name instanceOf String;  /* 检查name的类型是否是String，如果是，则值为真；否则为假 */
         
         a = 10;
         b = (a == 1)? 20 : 30;
