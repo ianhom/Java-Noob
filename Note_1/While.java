ec1b12eaa34099a741eb1621c66a2b16dea9a879
Ian Hom <ianhom@163.com>
2016-09-03 10:30:56 +0800

    Create While.java

---
+++ Note_1/While.java
@@ -0,0 +1,11 @@
+public class While{
+    public static void main(String args[]){
+        int x = 10;
+        while(x < 20){
+            System.out.println("Value of x : " + x);
+            x++;
+
+        }
+    }
+}
+/* 基本与C的while相同 */
