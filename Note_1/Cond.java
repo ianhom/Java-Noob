3a405e2b048b9b45a8f655b437165ff96975d8a6
Ian Hom <ianhom@163.com>
2016-09-03 09:20:24 +0800

    Create Cond.java

---
+++ Note_1/Cond.java
@@ -0,0 +1,14 @@
+public class Cond{
+    public String name = 'Ye';
+    public static void main(String args[]){
+        int a,b;
+        boolean c = name instanceOf String;
+        
+        a = 10;
+        b = (a == 1)? 20 : 30;
+        System.out.println("Value of b is: " + b);
+        b = (a == 10)? 20 : 30;
+        System.out.println("Value of b is: " + b);
+        Syetem.out.println("Value of c is: " + c);
+    }
+}
