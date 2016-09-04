3cd709a402c8714400e36c498e8e9ff5acbc7142
Ian Hom <ianhom@163.com>
2016-09-04 22:35:12 +0800

    Create For.java

---
+++ Note_1/For.java
@@ -0,0 +1,9 @@
+public class ForTest{
+    public static void main(String args[]){
+        for(int a = 0; a < 10; a++)
+        {
+            System.out.println("a is : " + a);
+        }
+    }
+}
+/* 与C的for相同 */
